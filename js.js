c8023e2ec110c57cf4cc97ddb86cb477e6bdbfd9
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-02-14 23:40:03 +0600

    practicing class

--- js.js
+++ js.js
@@ -1,64 +1,16 @@
-const form = document.getElementById('generate-form')
-const qr = document.getElementById('qrcode')
-
-const onGenerateSubmit = (e) =>{
-   e.preventDefault()
-
-   clearUI()
-
-   const url = document.getElementById('url').value;
-   const size = document.getElementById('size').value;
-
-   if(url === ''){
-      alert('Please enter a URL')
-   }else{
-      hideSpinner()
-
-      setTimeout(()=>{
-        hideSpinner()
-
-        generateQRCode(url, size)
-
-        setTimeout(()=>{
-          const saveUrl = qr.querySelector('img').src;
-          createSaveBtn(saveUrl)
-        }, 50)
-      }, 1000)
-   }
+class testDrive{
+    constructor(name){
+       console.log("Constructor: tumi " + name)
+    }
 }
 
-const generateQRCode = (url, size) =>{
-   const qrcode = new QRCode('qrcode', {
-      text: url,
-      width: size,
-      height: size
-   })
+class manger extends testDrive{
+    constructor(name){
+        super(name)
+        console.log("Constructor: Manager" + name)
+    }
 }
 
-const showSpinner = () =>{
-   document.getElementById('spinner').style.display = 'block'
-}
+let person2 = new manger('maymud')
 
-const hideSpinner = () =>{
-   document.getElementById('spinner').style.display = 'none'
-}
-
-const clearUI = () =>{
-   qr.innerHTML = '';
-   const saveLink = document.getElementById('save-link')
-   if(saveLink) saveLink.remove()
-}
-
-const createSaveBtn = (saveUrl) =>{
-   const link = document.createElement('a')
-   link.id = 'save-link';
-   link.href = saveUrl;
-   link.download = 'qrcode'
-   link.innerHTML = 'Save Image'
-   document.getElementById('generated').appendChild(link)
-}
-
-hideSpinner()
-
-form.addEventListener('submit', onGenerateSubmit)
-
+console.log(person2)
