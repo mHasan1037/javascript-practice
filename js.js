82494a3e85815b02813f3ddbbc2cdb2c44ae177f
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-03-30 18:33:15 +0600

    practised fetching data using redux

--- js.js
+++ js.js
@@ -1,17 +1,72 @@
-fetch('https://api.twelvedata.com/stocks')
-.then(res => res.json())
-.then(data => stockData(data))
-.catch(error => console.error(error))
+// const table = document.getElementById('table')
+// const form = document.querySelector('form')
+// const input = form.querySelector('input')
+
+// form.addEventListener('submit', e =>{
+//   e.preventDefault()
+//   const search = input.value
+
+//   stockData(search)
+//   input.value = ''
+//   table.innerHTML = 'Loading...'
+// })
+
+// function stockData(search){
+
+//   fetch('https://api.twelvedata.com/stocks')
+//   .then(res => res.json())
+//   .then(data => {
+
+//     if(Array.isArray(data.data)){
+//       const result = data.data.filter(stock =>{
+//         const stockName = stock.name.toLowerCase()
+//         return stockName.includes(search.toLowerCase())
+//       })
+//       if(result){
+//          table.innerHTML = `
+//          <tr>
+//             <th>Type</th>
+//             <th>Name</th>
+//             <th>Country</th>
+//             <th>Currency</th>
+//             <th>Exchange</th>
+//         </tr>
+//          `
+//           result.map((data, idx)=>{
+//             const {name, country, currency, exchange, type} = data
+    
+//             const box = document.createElement('tr')
+//             box.classList.add('box')
+//             box.innerHTML = `
+//             <td>${type}</td>
+//             <td>${name}</td>
+//             <td>${country}</td>
+//             <td>${currency}</td>
+//             <td>${exchange}</td>
+//             `
+//             table.appendChild(box)
+//           })
+//         }else{
+//           console.log('no stock found')
+//         }
+//       }
+//   })
+//   .catch(error => console.error(error))
+// }
 
 
-function stockData(data){
-  const search = "micro"
 
-  const foundArray = data.data.find(array => array.name.toLowerCase() === search)
 
-  if(foundArray){
-    console.log(foundArray)
-  }else{
-    console.log('Not Found')
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,144 @@
+// const {createStore} = require('redux')
+
+// const INCREMENT = 'INCREMENT'
+// const DECREMENT = 'DECREMENT'
+
+// const initialCounterState = {
+//     count: 0
+// }
+
+// const incrementCounter = () =>{
+//     return {
+//         type: INCREMENT
+//     }
+// }
+
+// const decrementCounter = () =>{
+//     return {
+//         type: DECREMENT
+//     }
+// }
+
+// const counterReducer = (state=initialCounterState, action) =>{
+//     switch(action.type){
+//         case INCREMENT:
+//             return{
+//                 ...state,
+//                 count: state.count + 1
+//             }
+
+//         case DECREMENT:
+//             return{
+//                 ...state,
+//                 count: state.count - 1
+//             }
+
+//         default:
+//             state;
+//     }
+// }
+
+// const store = createStore(counterReducer)
+
+// store.subscribe(()=>{
+//     console.log(store.getState())
+// })
+
+// store.dispatch(incrementCounter())
+
+const { default: axios } = require('axios')
+const {createStore, applyMiddleware} = require('redux')
+const thunk = require('redux-thunk').default
+
+const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST'
+const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS'
+const GET_TODOS_FAILED = 'GET_TODOS_FAILED'
+const API_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+
+const initialTodosState = {
+    todos: [],
+    isLoading: false,
+    error: null
+}
+
+
+const getTodosRequest = () =>{
+    return{
+        type: GET_TODOS_REQUEST
+    }
+}
+
+
+const getTodosSuccess = (todos) =>{
+    return{
+       type: GET_TODOS_SUCCESS,
+       payload: todos
+    }
+}
+
+
+const getTodosFailed = (error) =>{
+    return{
+        type: GET_TODOS_FAILED,
+        payload: error
+    }
+}
+
+
+const todosReducer = (state=initialTodosState, action) =>{
+    switch(action.type){
+        case GET_TODOS_REQUEST:
+            return{
+                ...state, 
+                isLoading: true
+            }
+        case GET_TODOS_SUCCESS:
+            return{
+                ...state,
+                isLoading: false,
+                todos: action.payload
+            }
+        case GET_TODOS_FAILED:
+            return{
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+
+const fetchData = () =>{
+  return(dispatch) =>{
+      dispatch(getTodosRequest())
+      axios.get(API_URL)
+      .then(res => {
+        const todos = res.data;
+        const titles = todos.map(todo => todo.title)
+        dispatch(getTodosSuccess(titles))
+      })
+      .catch(error =>{
+        const errorMessage = (error.message)
+        dispatch(getTodosFailed(errorMessage))
+      })
+  }
+}
+
+
+const store = createStore(todosReducer, applyMiddleware(thunk))
+
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+store.dispatch(fetchData())
+
+
+
+
+
+
+
