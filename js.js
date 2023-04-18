8ab508a0c0c149ace7f75ddb3da00e0486fbdf7b
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-04-18 23:37:57 +0600

    learning oop

--- js.js
+++ js.js
@@ -1,91 +1,204 @@
-const { default: axios } = require('axios')
-const {createStore, applyMiddleware} = require('redux')
-const thunk = require('redux-thunk').default
+// const { default: axios } = require('axios')
+// const {createStore, applyMiddleware} = require('redux')
+// const thunk = require('redux-thunk').default
 
 
-const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST'
-const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS'
-const GET_TODOS_FAILED = 'GET_TODOS_FAILED'
-const API_URL = 'https://jsonplaceholder.typicode.com/todos'
+// const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST'
+// const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS'
+// const GET_TODOS_FAILED = 'GET_TODOS_FAILED'
 
-const initialTodosState = {
-    isLoading: false,
-    todos: [],
-    error: null,
-}
 
-const getTodosRequest = () =>{
-    return{
-        type: GET_TODOS_REQUEST
-    }
-}
+// const initialTodosState = {
+//     isLoading: false,
+//     todos: [],
+//     error: null,
+// }
 
-const getTodosSuccess = (todos) =>{
-    return{
-        type: GET_TODOS_SUCCESS,
-        payload: todos
-    }
-}
+// const getTodosRequest = () =>{
+//     return{
+//         type: GET_TODOS_REQUEST
+//     }
+// }
 
-const getTodosFailed = (error) =>{
-    return{
-        type: GET_TODOS_FAILED,
-        payload: error
-    }
-}
+// const getTodosSuccess = (todos) =>{
+//     return{
+//         type: GET_TODOS_SUCCESS,
+//         payload: todos
+//     }
+// }
 
-const todosReducer = (state=initialTodosState, action) =>{
-    switch(action.type){
-        case GET_TODOS_REQUEST:
-            return{
-                ...state,
-                isLoading: true
-            }
-        case GET_TODOS_SUCCESS:
-            return{
-                ...state,
-                isLoading: false,
-                todos: action.payload
-            }
-        case GET_TODOS_FAILED:
-            return{
-                ...state,
-                isLoading: false,
-                error: action.payload
-            }
+// const getTodosFailed = (error) =>{
+//     return{
+//         type: GET_TODOS_FAILED,
+//         payload: error
+//     }
+// }
+
+// const todosReducer = (state=initialTodosState, action) =>{
+//     switch(action.type){
+//         case GET_TODOS_REQUEST:
+//             return{
+//                 ...state,
+//                 isLoading: true
+//             }
+//         case GET_TODOS_SUCCESS:
+//             return{
+//                 ...state,
+//                 isLoading: false,
+//                 todos: action.payload
+//             }
+//         case GET_TODOS_FAILED:
+//             return{
+//                 ...state,
+//                 isLoading: false,
+//                 error: action.payload
+//             }
         
-        default:
-            return state;
-    }
+//         default:
+//             return state;
+//     }
+// }
+
+// function fetchData(){
+//    return(dispatch) =>{
+//       dispatch(getTodosRequest())
+//       axios.get(API_URL)
+//       .then(res =>{
+//         const todos = res.data
+//         const titles = todos.map(todo => todo.title)
+//         dispatch(getTodosSuccess(titles))
+//       })
+//       .catch(error =>{
+//         const errorMessage = (error.message)
+//         dispatch(getTodosFailed(errorMessage))
+//         console.log(error.message)
+//       })
+//    }
+// }
+
+
+// const store = createStore(todosReducer, applyMiddleware(thunk))
+
+// store.subscribe(()=>{
+//     console.log(store.getState())
+// })
+
+// store.dispatch(fetchData())
+
+
+
+
+
+
+
+
+// function BankAccount(customerName, balance = 0){
+//    this.customerName = customerName;
+//    this.accountNumber = Date.now()
+//    this.balance = balance
+
+//    // this.deposite = function(amount){
+//    //     this.balance += amount
+//    // }
+
+//    // this.withdraw = function(amount){
+//    //    this.balance -= amount
+//    // }
+// }
+
+
+
+// const accounts = [];
+// const accountForm = document.querySelector('#accountForm');
+// const customerName = document.querySelector('#customerName');
+// const balance = document.querySelector('#balance');
+
+// const depositForm = document.querySelector('#depositeForm');
+// const accountNumber = document.querySelector('#accountNum');
+// const amount = document.querySelector('#amount');
+
+
+// accountForm.addEventListener('submit', (e)=>{
+//    e.preventDefault()
+//    const account = new BankAccount(customerName.value, +balance.value)
+//    accounts.push(account)
+
+//    customerName.value = ''
+//    balance.value = ''
+
+
+//    console.log(accounts)
+// })
+
+
+// depositForm.addEventListener('submit', (e)=>{
+//    e.preventDefault()
+//    const account = accounts.find(account => account.accountNumber === +accountNumber.value)
+//    account.deposite(+amount.value)
+
+//    console.log(accounts)
+
+//    accountNumber.value = ''
+//    amount.value = ''
+// })
+
+
+
+class BankAccount{
+     accountName;
+     accountNumber;
+     balance
+
+     constructor(accountName, balance = 0){
+        this.accountName = accountName;
+        this.accountNumber = Date.now();
+        this.balance = balance
+     }
+
+     deposite(amount){
+        this.balance += amount
+     }
+
+     withdraw(amount){
+        this.balance -= amount
+     }
 }
 
-function fetchData(){
-   return(dispatch) =>{
-      dispatch(getTodosRequest())
-      axios.get(API_URL)
-      .then(res =>{
-        const todos = res.data
-        const titles = todos.map(todo => todo.title)
-        dispatch(getTodosSuccess(titles))
-      })
-      .catch(error =>{
-        const errorMessage = (error.message)
-        dispatch(getTodosFailed(errorMessage))
-        console.log(error.message)
-      })
+
+class CurrentAccount extends BankAccount{
+   transactionLimit = 5000;
+
+   constructor(customerName, balance = 0){
+      super(customerName, balance)
+   }
+
+   takeBusinessLoan(amount){
+      console.log('taking business load: ' + amount)
    }
 }
 
 
-const store = createStore(todosReducer, applyMiddleware(thunk))
+class SavingAccount extends BankAccount{
+   transactionLimit = 1000;
 
-store.subscribe(()=>{
-    console.log(store.getState())
-})
+   constructor(customerName, balance = 0){
+      super(customerName, balance)
+   }
 
-store.dispatch(fetchData())
+   takePersonalLoan(amount){
+      console.log('taking personal load: ' + amount)
+   }
+}
 
 
+
+const hasanAcc = new SavingAccount('Hasan', 1000)
+
+
+hasanAcc.withdraw(3000)
+hasanAcc.takePersonalLoan(1000)
+
+console.log(hasanAcc)
 
 
 
--- script.js
+++ script.js
@@ -46,89 +46,8 @@
 
 // store.dispatch(incrementCounter())
 
-const { default: axios } = require("axios")
-const { applyMiddleware, createStore } = require("redux")
-const thunk = require("redux-thunk").default
-
-const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST'
-const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS'
-const GET_TODOS_FAILED = 'GET_TODOS_FAILED'
-const API_URL = 'https://jsonplaceholder.typicode.com/todos'
-
-const initialState = {
-    isLoading: false,
-    todos: [],
-    error: null
-}
-
-const getTodosRequest = () =>{
-    return{
-        type: GET_TODOS_REQUEST
-    }
-}
-
-const getTodosSuccess = (todos) =>{
-    return{
-        type: GET_TODOS_SUCCESS,
-        payload: todos
-    }
-}
-
-const getTodosFailed = (error) =>{
-    return {
-        type: GET_TODOS_FAILED,
-        payload: error
-    }
-}
-
-const createReducer = (state=initialState, action) =>{
-    switch(action.type){
-        case GET_TODOS_REQUEST:
-            return{
-                ...state,
-                isLoading: true
-            }
-        case GET_TODOS_SUCCESS:
-            return{
-                ...state,
-                isLoading: false,
-                todos: action.payload,
-            }
-        case GET_TODOS_FAILED:
-            return{
-                isLoading: false,
-                todos: [],
-                error: action.payload
-            }
-        default:
-            return state
-    }
-}
-
-function fetchData(){
-    return(dispatch)=>{
-        dispatch(getTodosRequest())
-        axios.get(API_URL)
-        .then(res =>{
-            const todos = res.data
-            const titles = todos.map(todo => todo.title)
-            dispatch(getTodosSuccess(titles))
-        })
-        .catch(error =>{
-            const errorMessage = (error.message)
-            dispatch(getTodosFailed(errorMessage))
-        })
-    }
-}
 
 
-const store = createStore(createReducer, applyMiddleware(thunk))
-
-store.subscribe(()=>{
-    console.log(store.getState())
-})
-
-store.dispatch(fetchData())
 
 
 
