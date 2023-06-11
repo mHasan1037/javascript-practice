22ccd8a3b90d1116a600b1c7bc887e90efdf3444
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-06-11 23:25:20 +0600

    practiced some array methods

--- script.js
+++ script.js
@@ -50,101 +50,28 @@
 
 
 
+const indices = []
 
-let pageNo = 1;
-let results = 10;
-let isLoading = true;
+const array = ["a", "b", "a", "c", "b", "a", "d", "e"];
 
-const container = document.querySelector('.container')
-const getLastUserEl = () => document.querySelector('.container > .person:last-child')
-
-const loadingEle = document.querySelector('#loading')
-const toggleLoading = (isLoading) =>{
-    loadingEle.classList.toggle('show', isLoading)
-}
-
-async function fetchData(pageNo, results){
-     const res = await fetch(`https://randomuser.me/api/?page=${pageNo}&results=${results}&seed=abc`)
-     const data = await res.json()
-
-     return data && data.results
-}
+const nums = [1, 5, 4, 6, 4, 8, 7, 10]
 
 
-const loadUsers = (pageNo, results) =>{
-    return new Promise((resolve, reject)=>{
-        fetchData(pageNo, results).then((data)=>{
-            makeUi(data)
-            if(isLoading){
-                observeLastUser()
-                isLoading = false
-            }
-           
-            resolve('rendering completed')
-        }).catch((error)=>{
-            reject(error)
-        })
-    })
-    
-}
+// let idx = array.indexOf(element)
 
-toggleLoading(true)
-loadUsers(pageNo, results)
-.then((data)=>{
-    toggleLoading(false)
-}).catch((error)=>{
-    toggleLoading(false)
+// while(idx !== -1){
+//     indices.push(idx)
+
+//     idx = array.indexOf(element, idx + 1)
+// }
+
+
+
+let ans = array.fliter((ele, idx)=>{
+    return array.indexOf(ele) === idx
 })
 
-
-function makeUi(data){
-    data.forEach((person)=>{
-        const {gender, name: {first, last}, picture: {medium: profileImg}} = person
-
-        const personEl = document.createElement('div')
-        personEl.classList.add('person')
-        personEl.innerHTML = `
-            <h3>${first + last}</h3>
-            <p>${gender}</p>
-            <img src=${profileImg} alt=${first + last}/>
-        `
-        container.appendChild(personEl) 
-    })
-}
-
-let options = {
-    root: null,
-    rootMargin: '-20px',
-    threshold: 0.5
-}
-
-const observer = new IntersectionObserver(callbackFunc, options)
-
-function callbackFunc(entries, observer){
-      const entry = entries[0]
-
-      if(!entry.isIntersecting) return
-
-      pageNo += 1
-      loadUsers(pageNo, results).then((res)=>{
-        observeLastUser()
-        toggleLoading(false)
-      }).catch((error)=>{
-        toggleLoading(true)
-      })
-      
-    observer.unobserve(entry.target)
-}
-
-const observeLastUser = () =>{
-    observer.observe(getLastUserEl())
-}
-
-
-
-
-
-
+console.log(ans)
 
 
 
